fb92cac8b5cef2814b7598c28c11c11f8990aba1
devopmega <adeshsingh3002@gmail.com>
2026-08-12 00:12:48 +0530

    Initial Commit

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ import userRouter from "./modules/user/routes/user.route.js";
 import { systemHealth } from "./utils/systemHealth.js";
 import { undeclaredRouteHandler } from "./utils/undeclaredRoute.js";
 
+
 const app = express();
 
 app.disable("x-powered-by");
@@ -22,7 +23,7 @@ app.use(helmet());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 app.use(cookieParser());
-
+console.log(process.env.ALLOWED_ORIGIN)
 const allowedOrigins = [
   process.env.ALLOWED_ORIGIN
 ];
